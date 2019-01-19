d0e9557b9ee6a53a072d45a0a679af9705ce03fb
Piotr Jura <>
2019-01-19 14:16:16 +0100

    #0001

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+
+@section('content')
+<h1>Contact</h1>
+<p>Hello this is contact!</p>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+<h1>Welcome to Laravel!</h1>
+
+<p>This is the content of the main page!</p>
+@endsection
