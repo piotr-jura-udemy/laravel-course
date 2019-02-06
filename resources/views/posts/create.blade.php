78d86e92818cc118da4019038bdaf7345d52a0d6
Piotr Jura <>
2019-02-06 19:31:15 +0100

    #0023

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,25 +3,8 @@
 @section('content')
     <form method="POST" action="{{ route('posts.store') }}">
         @csrf
-        <p>
-            <label>Title</label>
-            <input type="text" name="title" value="{{ old('title') }}"/>
-        </p>
         
-        <p>
-            <label>Content</label>
-            <input type="text" name="content" value="{{ old('content') }}"/>
-        </p>
-
-        @if($errors->any())
-            <div>
-                <ul>
-                    @foreach($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+        @include('posts._form')
 
         <button type="submit">Create!</button>
     </form>
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -6,27 +6,7 @@
         @csrf
         @method('PUT')
 
-        <p>
-            <label>Title</label>
-            <input type="text" name="title" 
-                value="{{ old('title', $post->title) }}"/>
-        </p>
-        
-        <p>
-            <label>Content</label>
-            <input type="text" name="content" 
-                value="{{ old('content', $post->content) }}"/>
-        </p>
-
-        @if($errors->any())
-            <div>
-                <ul>
-                    @foreach($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+        @include('posts._form')
 
         <button type="submit">Update!</button>
     </form>
