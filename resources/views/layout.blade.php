a386954f9c25612784a82fbc95dd1a43dadd9a8b
Piotr Jura <>
2019-02-10 14:04:32 +0100

    #0026 Laravel mix setup

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <title>Document</title>
 </head>
 <body>
@@ -21,5 +22,7 @@
     @endif
 
     @yield('content')
+
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
