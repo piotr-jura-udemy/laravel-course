6b9ce3eb02079fc23fded167363ac9b26fe12983
Piotr Jura <>
2019-02-16 09:49:20 +0100

    #0032 Testing store with valid params

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -35,4 +35,18 @@ class PostTest extends TestCase
             'title' => 'New title'
         ]);
     }
+
+    public function testStoreValid()
+    {
+        $params = [
+            'title' => 'Valid title',
+            'content' => 'At least 10 characters'
+        ];
+
+        $this->post('/posts', $params)
+            ->assertStatus(302)
+            ->assertSessionHas('status');
+
+        $this->assertEquals(session('status'), 'Blog post was created!');
+    }
 }
