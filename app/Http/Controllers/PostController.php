06864854e8ceb50ae26eb54cc566fbe91823892e
Piotr Jura <>
2019-03-10 15:11:42 +0100

    #0050 findOrFail with relations, displaying a list of comments

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -46,7 +46,9 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        return view('posts.show', ['post' => BlogPost::findOrFail($id)]);
+        return view('posts.show', [
+            'post' => BlogPost::with('comments')->findOrFail($id)
+        ]);
     }
 
     public function create()
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,4 +9,17 @@
     @if ((new Carbon\Carbon())->diffInMinutes($post->created_at) < 5 )
         <strong>New!</strong>
     @endif
+
+    <h4>Comments</h4>
+
+    @forelse($post->comments as $comment)
+        <p>
+            {{ $comment->content }}
+        </p>
+        <p class="text-muted">
+            added {{ $comment->created_at->diffForHumans() }}
+        </p>
+    @empty
+        <p>No comments yet!</p>
+    @endforelse
 @endsection('content')
