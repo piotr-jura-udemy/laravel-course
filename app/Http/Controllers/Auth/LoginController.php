afc04b6373c1ec96f88a3165ed6b218363226136
Piotr Jura <>
2019-03-16 14:07:49 +0100

    #0057 Login form

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -36,4 +36,9 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    // public function username()
+    // {
+    //     return 'username';
+    // }
 }
