58b558f7b23f9e5a09c5f5b48289004969748d0e
Piotr Jura <>
2019-03-17 19:03:42 +0100

    #0059 Retrieve current User

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+
 class HomeController extends Controller
 {
     public function home()
     {
+        // dd(Auth::check());
+        // dd(Auth::id());
+        // dd(Auth::user());
         return view('home');
     }
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -24,7 +24,7 @@
             @else
                 <a class="p-2 text-dark" href="{{ route('logout') }}"
                     onclick="event.preventDefault();document.getElementById('logout-form').submit();"
-                    >Logout</a>
+                    >Logout ({{ Auth::user()->name }})</a>
 
                 <form id="logout-form" action={{ route('logout') }} method="POST"
                     style="display: none;">
