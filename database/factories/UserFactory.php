c3647375943fe53b814f79c87bfa78683b69de17
Piotr Jura <>
2019-03-30 18:44:55 +0100

    #0065 Using Model Factory in Database Seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,3 +22,10 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->state(App\User::class, 'john-doe', function (Faker $faker) {
+    return [
+        'name' => 'John Doe',
+        'email' => 'john@laravel.test',
+    ];
+});
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,12 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        DB::table('users')->insert([
-            'name' => 'John Doe',
-            'email' => 'john@laravel.test',
-            'email_verified_at' => now(),
-            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-            'remember_token' => str_random(10),
-        ]);
+        factory(App\User::class)->states('john-doe')->create();
+        factory(App\User::class, 20)->create();
     }
 }
