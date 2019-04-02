ab8585a40af32f5742731b3aec64b85dc6bca909
Piotr Jura <>
2019-04-02 18:33:43 +0200

    #0069 Subscribing to deleting event on BlogPost model

--- app/BlogPost.php
+++ app/BlogPost.php
@@ -14,4 +14,13 @@ class BlogPost extends Model
     {
         return $this->hasMany('App\Comment');
     }
+    
+    public static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function (BlogPost $blogPost) {
+            $blogPost->comments()->delete();
+        });
+    }
 }
