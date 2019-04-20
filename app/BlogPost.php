d6f60720abfe432d73fa068955ef9c2a3a644188
Piotr Jura <>
2019-04-20 18:13:33 +0200

    #111 Defining ManyToMany relation on BlogPost and Tag models

--- app/BlogPost.php
+++ app/BlogPost.php
@@ -26,6 +26,11 @@ class BlogPost extends Model
         return $this->belongsTo('App\User');
     }
 
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag');
+    }
+
     public function scopeLatest(Builder $query)
     {
         return $query->orderBy(static::CREATED_AT, 'desc');
