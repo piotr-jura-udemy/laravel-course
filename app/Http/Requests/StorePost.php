8877c4066fb985f7e397b452619e6d12409f57a2
Piotr Jura <>
2019-04-28 14:13:22 +0200

    #133 Upload validation

--- app/Http/Requests/StorePost.php
+++ app/Http/Requests/StorePost.php
@@ -25,7 +25,8 @@ class StorePost extends FormRequest
     {
         return [
             'title' => 'bail|min:5|required|max:100',
-            'content' => 'required|min:10'
+            'content' => 'required|min:10',
+            'thumbnail' => 'image|mimes:jpg,jpeg,png,gif,svg|max:1024|dimensions:min_height=500'
         ];
     }
 }
