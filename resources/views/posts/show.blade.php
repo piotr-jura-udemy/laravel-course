b742ad695ade7424c4b7242d4c2162bc8fe15117
Piotr Jura <>
2019-04-28 13:28:24 +0200

    #131 Styling the image

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,16 +3,24 @@
 @section('content')
 <div class="row">
     <div class="col-8">
-        <h1>
+        @if($post->image)
+        <div style="background-image: url('{{ $post->image->url() }}'); min-height: 500px; color: white; text-align: center; background-attachment: fixed;">
+            <h1 style="padding-top: 100px; text-shadow: 1px 2px #000">
+        @else
+            <h1>
+        @endif
             {{ $post->title }}
             @badge(['show' => now()->diffInMinutes($post->created_at) < 30])
                 Brand new Post!
             @endbadge
-        </h1>
+        @if($post->image)    
+            </h1>
+        </div>
+        @else
+            </h1>
+        @endif
 
         <p>{{ $post->content }}</p>
-
-        <img src="{{ $post->image->url() }}" />
 
         @updated(['date' => $post->created_at, 'name' => $post->user->name])
         @endupdated
