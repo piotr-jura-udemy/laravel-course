5390492b8af41f00e0d27e327d5c1df31f34936a
Piotr Jura <>
2019-04-28 13:47:14 +0200

    #132 Deleting files

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -145,6 +145,21 @@ class PostController extends Controller
         $validatedData = $request->validated();
 
         $post->fill($validatedData);
+
+        if ($request->hasFile('thumbnail')) {
+            $path = $request->file('thumbnail')->store('thumbnails');
+
+            if ($post->image) {
+                Storage::delete($post->image->path);
+                $post->image->path = $path;
+                $post->image->save();
+            } else {
+                $post->image()->save(
+                    Image::create(['path' => $path])
+                );
+            }
+        }
+
         $post->save();
         $request->session()->flash('status', 'Blog post was updated!');
 
