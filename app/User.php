55bfb6ed788f6e4080381787f542e9353d67e3c3
Piotr Jura <>
2019-05-03 08:40:56 +0200

    #143 One to Many Polymorphic migration and relations

--- app/User.php
+++ app/User.php
@@ -38,6 +38,11 @@ class User extends Authenticatable
         return $this->hasMany('App\Comment');
     }
 
+    public function commentsOn()
+    {
+        return $this->morphMany('App\Comment', 'commentable')->latest();
+    }
+
     public function image()
     {
         return $this->morphOne('App\Image', 'imageable');
