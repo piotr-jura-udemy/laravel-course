ddd6cf2d1e045726363b08b267b473b588e8b198
Piotr Jura <>
2019-05-05 20:14:33 +0200

    #154 CommentPosted mailable, creating the views for email

---
+++ app/Mail/CommentPosted.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Comment;
+
+class CommentPosted extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $comment;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Comment $comment)
+    {
+        $this->comment = $comment;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $subject = "Commented was posted on your {$this->comment->commentable->title} blog post";
+        return $this->subject($subject)
+            ->view('emails.posts.commented');
+    }
+}
