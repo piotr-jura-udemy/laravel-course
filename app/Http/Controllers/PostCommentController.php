e1fee7d47b5a2ab08113da0186b72705008efd78
Piotr Jura <>
2019-05-05 19:24:05 +0200

    Sending e-mail

--- app/Http/Controllers/PostCommentController.php
+++ app/Http/Controllers/PostCommentController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreComment;
 use App\BlogPost;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\PostCommented;
 
 class PostCommentController extends Controller
 {
@@ -15,10 +17,14 @@ class PostCommentController extends Controller
     public function store(BlogPost $post, StoreComment $request)
     {
         // Comment::create()
-        $post->comments()->create([
+        $comment = $post->comments()->create([
             'content' => $request->input('content'),
             'user_id' => $request->user()->id
         ]);
+
+        Mail::to($post->user)->send(
+            new PostCommented($comment)
+        );
 
         return redirect()->back()
             ->withStatus('Comment was created!');
--- config/mail.php
+++ config/mail.php
@@ -60,6 +60,11 @@ return [
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
+    'to' => [
+        'address' => 'jura.piotrek@gmail.com',
+        'name' => 'Piotr Jura'
+    ],
+
     /*
     |--------------------------------------------------------------------------
     | E-Mail Encryption Protocol
