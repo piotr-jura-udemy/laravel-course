e64f38639b7486d6815bae8553a17645515d14b5
Piotr Jura <>
2019-05-13 18:57:05 +0200

    #179 Using JSON for storing translations

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,5 +10,8 @@
 <p>{{ trans_choice('messages.plural', 1, ['a' => 1]) }}</p>
 <p>{{ trans_choice('messages.plural', 2, ['a' => 1]) }}</p>
 
+<p>Using JSON: {{ __('Welcome to Laravel!') }}</p>
+<p>Using JSON: {{ __('Hello :name', ['name' => 'Piotr']) }}</p>
+
 <p>This is the content of the main page!</p>
 @endsection
