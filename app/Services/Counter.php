e9811f7f86b5f0067279378c9c21349e917cfea9
Piotr Jura <>
2019-05-17 19:17:28 +0200

    #185 Fix

--- app/Services/Counter.php
+++ app/Services/Counter.php
@@ -6,6 +6,13 @@ use Illuminate\Support\Facades\Cache;
 
 class Counter
 {
+    private $timeout;
+
+    public function __construct(int $timeout)
+    {
+        $this->timeout = $timeout;
+    }
+
     public function increment(string $key, array $tags = null): int
     {
         $sessionId = session()->getId();
@@ -18,7 +25,7 @@ class Counter
         $now = now();
 
         foreach ($users as $session => $lastVisit) {
-            if ($now->diffInMinutes($lastVisit) >= 1) {
+            if ($now->diffInMinutes($lastVisit) >= $this->timeout) {
                 $diffrence--;
             } else {
                 $usersUpdate[$session] = $lastVisit;
@@ -27,7 +34,7 @@ class Counter
 
         if(
             !array_key_exists($sessionId, $users)
-            || $now->diffInMinutes($users[$sessionId]) >= 1
+            || $now->diffInMinutes($users[$sessionId]) >= $this->timeout
         ) {
             $diffrence++;
         }
