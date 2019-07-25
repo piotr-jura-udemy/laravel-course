faec8cefc7bfd99e417e9e7fc9c20a02d169caa1
Piotr Jura <>
2019-07-25 18:56:51 +0200

    #200 Defining API routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,16 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->name('api.v1.')->namespace('Api\V1')->group(function () {
+    Route::get('/status', function () {
+        return response()->json(['status' => 'OK']);
+    })->name('status');
+    Route::apiResource('posts.comments', 'PostCommentController');
+});
+
+Route::prefix('v2')->name('api.v2.')->group(function () {
+    Route::get('/status', function () {
+        return response()->json(['status' => true]);
+    })->name('status');
+});
