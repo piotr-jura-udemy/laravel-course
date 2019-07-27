66c1e1ffe3dc9aa48206684e56e6f0702dcd8a19
Piotr Jura <>
2019-07-27 19:48:18 +0200

    #204 Storing a resource

--- app/Http/Controllers/Api/V1/PostCommentController.php
+++ app/Http/Controllers/Api/V1/PostCommentController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\BlogPost;
 use App\Http\Resources\Comment as CommentResource;
+use App\Http\Requests\StoreComment;
 
 class PostCommentController extends Controller
 {
@@ -16,7 +17,7 @@ class PostCommentController extends Controller
      */
     public function index(BlogPost $post, Request $request)
     {
-        $perPage = (int) $request->input('per_page') ?? 15;
+        $perPage = $request->input('per_page') ?? 15;
         return CommentResource::collection(
             $post->comments()->with('user')->paginate($perPage)->appends(
                 [
@@ -32,9 +33,13 @@ class PostCommentController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(BlogPost $post, StoreComment $request)
     {
-        //
+        $comment = $post->comments()->create([
+            'content' => $request->input('content'),
+            'user_id' => $request->user()->id
+        ]);
+        event(new CommentPosted($comment));
     }
 
     /**
