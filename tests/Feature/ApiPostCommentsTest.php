23e266aeb61d3860acf8097911833fe301d49564
Piotr Jura <>
2019-08-10 13:23:54 +0200

    #210 Testing JSON structure deeply

--- tests/Feature/ApiPostCommentsTest.php
+++ tests/Feature/ApiPostCommentsTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\BlogPost;
+use App\Comment;
 
 class ApiPostCommentsTest extends TestCase
 {
@@ -22,5 +23,41 @@ class ApiPostCommentsTest extends TestCase
         $response->assertStatus(200)
             ->assertJsonStructure(['data', 'links', 'meta'])
             ->assertJsonCount(0, 'data');
+    }
+
+    public function testBlogPostHas10Comments()
+    {
+        factory(BlogPost::class)->create([
+            'user_id' => $this->user()->id
+        ])->each(function (BlogPost $post) {
+            $post->comments()->saveMany(
+                factory(Comment::class, 10)->make([
+                    'user_id' => $this->user()->id
+                ])
+            );
+        });
+
+        $response = $this->json('GET', 'api/v1/posts/2/comments');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(
+                [
+                    'data' => [
+                        '*' => [
+                            'id',
+                            'contnt',
+                            'created_at',
+                            'updated_at',
+                            'user' => [
+                                'id',
+                                'name'
+                            ]
+                        ]
+                    ],
+                    'links',
+                    'meta'
+                ]
+            )
+            ->assertJsonCount(10, 'data');
     }
 }
